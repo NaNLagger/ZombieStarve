5ae7dc3c4ab0b480b8e4c12317fb38e2a630f5a4
NaNLagger <stepan.lyashenko@gmail.com>
2016-02-10 19:02:31 +0600

    switch to pixels settings, added pixel perfect

---
+++ Assets/Scripts/Core/Utils/View/PixelPerfect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera)), ExecuteInEditMode]
+public class PixelPerfect : MonoBehaviour {
+
+    public float minOrtho = 7;
+    public int PPU = 32;
+    private int currentResolution;
+
+    void Awake() {
+        UpdateOrthoSize();
+    }
+
+    void Update() {
+#if UNITY_EDITOR
+        if (currentResolution != Screen.height)
+            UpdateOrthoSize();
+#endif
+    }
+
+    void UpdateOrthoSize() {
+        currentResolution = Screen.height;
+        float orthoSize = (currentResolution / (float)PPU) * 0.5f;
+        orthoSize = orthoSize >= minOrtho * 2 ? orthoSize % minOrtho : orthoSize;
+        GetComponent<Camera>().orthographicSize = orthoSize;
+    }
+}
